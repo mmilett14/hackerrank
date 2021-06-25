ccde01f27d66fbb7fe8de2e9ef7ef4e57bf2653d
mike <78767272+mmilett14@users.noreply.github.com>
2021-06-25 12:47:13 -0500

    incorrect solution

---
+++ 7-mini-max-sum.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'miniMaxSum' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def miniMaxSum(arr):
+    min_sum = sum(arr)
+    max_sum = sum(arr[0:3])
+    
+    for i in range(0, len(arr)):
+        current_sum = sum(arr) - arr[i]
+        
+        if current_sum < min_sum:
+            min_sum == current_sum
+        
+        if current_sum > max_sum:
+            max_sum == current_sum
+    
+    print(min_sum, max_sum)
+        
+    
+    
+if __name__ == '__main__':
+
+    arr = list(map(int, input().rstrip().split()))
+
+    miniMaxSum(arr)
