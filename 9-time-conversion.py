a440377abc134c18649a3875b6896e28636a9f45
mike <78767272+mmilett14@users.noreply.github.com>
2021-06-29 19:21:54 -0500

    problem solved

--- 9-time-conversion.py
+++ 9-time-conversion.py
@@ -14,8 +14,23 @@ import sys
 #
 
 def timeConversion(s):
+    min_sec = s[2:8]
     fmt = s[8:10]
     hour = int(s[0:2])
+
+
+    if fmt == 'AM' and hour == 12:
+        time = '00' + min_sec
+        return time
+
+    elif (fmt == 'AM' and hour > 0) or (fmt == 'PM' and hour == 12):
+        time = s[0:8]
+        return time
+
+    elif fmt == 'PM' and hour > 0 and hour < 12:
+        hour = hour + 12
+        time = str(hour) + min_sec
+        return time
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
