1dd60ec1afe66bffc8fbc123760eda47d7f2744b
mike <78767272+mmilett14@users.noreply.github.com>
2021-09-24 11:11:56 -0500

    closer? but also more complex

--- 17-two-characters.py
+++ 17-two-characters.py
@@ -1,4 +1,4 @@
-def alternate(s):
+def letter_count_dict(s):
     
     letter_count = {}
     
@@ -9,13 +9,22 @@ def alternate(s):
         
         else:
             letter_count[s[i]] += 1
-            
-          
+    
+    return letter_count
+
+
+def letter_count_replace(s, letter_count):
+           
     for letter in letter_count:
         
         if letter_count[letter] == 1:
             s = s.replace(letter, '')
+            
+    return s
+            
 
+def same_letter_replace(s):
+    
     left = 0
     
     while left < len(s)-1:
@@ -30,4 +39,36 @@ def alternate(s):
         else:
             left += 1
             
-    return len(s)
+    return s
+
+
+def letter_count_list(letter_count):
+    
+    letter_list = []
+
+    for letter in letter_count:
+        letter_list.append(letter)
+
+    return letter_list
+    
+
+def alternate(s):
+    
+    letter_count = letter_count_dict(s)
+    
+    s = letter_count_replace(s, letter_count)
+
+    s = same_letter_replace(s)
+    
+    letter_count = letter_count_dict(s)
+    
+    letter_list = letter_count_list(letter_count)
+    
+    if len(letter_list) == 2:
+        return len(s)
+    
+    else:
+        for i in range(0, len(letter_list)-2):
+            s = s.replace(letter_list[i], '')
+        
+        return len(s)
